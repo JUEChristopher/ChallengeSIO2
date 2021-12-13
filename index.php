e16b3279dee7de7f0d947a47f14ba3885dbaa00f
Teddy CLEMENT DELS <teddy.clement_dels@outlook.fr>
2021-12-13 14:27:00 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
 <?php 
-session_start();
-$_SESSION['user'] = [
-  'username' => 'John',
-  'password' => '0000'
-];
+
 $title = "Page d'accueil";
 require 'elements/header.php';
 ?>
 <div class="starter-template">
-  <h1>Bootstrap starter template</h1>
-  <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+  <h1>Bienvenue le site de challenge du groupe 2</h1>
+  <p class="lead">blabla.<br> blabla.</p>
 </div>
 
 <?php require 'elements/footer.php'; ?>
