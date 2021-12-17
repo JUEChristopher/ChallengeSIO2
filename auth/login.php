4b04f45a4c6a985f66717108cfcc2e02a497605a
Teddy CLEMENT DELS <teddy.clement_dels@outlook.fr>
2021-12-17 01:48:31 +0100

    Maj 1.3
    Login et register FINI Teddy

--- auth/login.php
+++ auth/login.php
@@ -1,85 +1,79 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="./style_login.css">
-    <link rel="stylesheet" href="https://use.typekit.net/kma0may.css">
-</head>
-<body>
-    
+
+<?php require_once "../elements/header.php"; ?>
+<?php require_once "../db_link/config.php"; ?>
+
+
+<!------------------- HTML ------------------->
+
+
+<div class="login_content">
     <div class="card_login">
         <h1>Connexion</h1>
-
-        <form method="post">
-
-            
-            <div  class="inputs">
+        <form method="post" class="card_login_content">
+            <div class="inputs">
                 <!-- champs Email -->
                 
                 <div class="input_email">
-                    <p class="choose_email" id="choose_email"> Email</p>    
+                    <p class="choose_email" id="choose_email">Email</p>    
                     <input type="email" name="login_email">
                 </div>
+
                 <!-- champs Password -->                
 
-                
                 <div class="choose_password">   
-                    <p class="choose_password" id="choose_password"> Mot de passe </p>
+                    <p class="choose_password" id="choose_password">Mot de passe</p>
                     <input type="password" name="login_password">
                 </div>
-                <!-- champs bouton d'inscription -->     
-                <div class="submit_button" >
-                    
-                    <button type="submit" name="login_submit"> Connexion</button>
-                    <P class="connexion" id="submit_connexion"> Pas encore de compte ? S'inscrire </P>
-                    
-                </div>
 
-                
+                <!-- champs bouton d'inscription -->  
+
+                <div class="submit_button" >
+                    <button type="submit" name="login_submit">Connexion</button>
+                    <p class="connexion" id="submit_connexion">Pas encore de compte ? S'inscrire</p>
+                </div>
             </div>
         </form>
     </div>
-        
-   
-</body>
+</div>
 
-</html>
+
+<!------------------- HTML ------------------->
+
 <?php
-require_once "../db_link/config.php";
+if (isset($_POST['login_submit'])){
 
+    $verif_email = $pdo->prepare('SELECT USER_ID, USER_ROLE, USER_PASSWORD FROM user WHERE USER_EMAIL = :mail');
+    $verif_email ->execute([':mail' => $_POST['login_email']]);
+    $resultat = $verif_email->fetch();
 
-$verif_email = $pdo->query('SELECT USER_EMAIL FROM user WHERE USER_ID="1"');
-$résultat_2 = $verif_email->fetchAll();
-
-
-$verif_password = $pdo->query('SELECT USER_PASSWORD FROM user');
-$résultat = $verif_password->fetchAll();
-print_r($résultat[0]->USER_PASSWORD);
-
-
-$error = null;
-
-try{
-    if (isset(($_POST['login_submit']))){
-         if (($_POST['login_email'] === $verif_email) && ($_POST['login_password'] === $verif_password)){
-             echo("MIIIIIIIIIIAAAAAAAAAAAAAAAAAAM");
-
-            header('location: index.php');
-            exit();
-
+    try{
+        if (password_verify($_POST['login_password'], $resultat->USER_PASSWORD))
+    {
+        session_start();
+        $_SESSION['role'] = $resultat->USER_ROLE;
+        $_SESSION['id_user'] = $resultat->USER_ID;
+        if($resultat->USER_ROLE === 'Etudiant'){
+            header('Location: ../index.php');
+        }
     }else{
         $erreur = "Erreur1";
     }
-   
+
+    } catch (PDOException $e) {
+        $error = $e->getMessage();
+    }
 }
 
-} catch (PDOException $e) {
-    $error = $e->getMessage();
-}
+
+
 ?>
+
 <?php if (isset($erreur)): ?>
 <div class="alert alert-danger">
     <?= $erreur?>
 </div>
+
 <?php endif ?>
 
+
+<?php require_once "../elements/footer.php" ?>
--- auth/register.php
+++ auth/register.php
@@ -1,90 +1,84 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="./style_register.css">
-    <link rel="stylesheet" href="https://use.typekit.net/kma0may.css">
-</head>
-<body>
-    
-    <div class="card_register">
-        <h1>Inscription</h1>
 
-        <form class="card_register_content" method="post">
+<?php require_once "../elements/header.php"; ?>
+<?php require_once "../db_link/config.php"; ?>
+
+
+<!------------------- HTML ------------------->
+
+<div class="card_register">
+    <h1>Inscription</h1>
+
+    <form class="card_register_content" method="post">
+        <div class="inputs_left">
+            <!-- champs Nom -->
+
+            <div class="input_last_name" id="class_choose_nom"> 
+                <p class="choose_Nom">Nom</p>    
+                <input type="last_name" name="register_last_name">
+            </div>
+
+            <!-- champs Prénom -->
+            
+            <div class="input_first_name" id="class_choose_first_name">
+                <p class="choose_first_name">Prénom</p>    
+                <input type="first_name" name="register_first_name">
+            </div>
+
+            <!-- champs Email -->
+            
+            <div class="input_email_register" id="class_choose_email_register">
+                <p class="choose-email_register" id="choose_email_register">Email</p>    
+                <input type="email" name="register_email">
+            </div>                
+
+            <!-- champs Classe -->
+            
+            <div class="input_class" id="class_choose_class" >
+                <p class="choose_class">Classe</p>    
+                <input type="class" name="register_class">
+            </div>
+        </div>
+        <div class="div_vertical_line">
+            <span class="vertical_line"></span>
+        </div>
+
+        
+        <div class="inputs_right"> 
+            <!-- champs Password -->                
+            
+            <div class="input_password_register" id="class_choose_password_register">   
+                <p class="choose_password_register" id="choose_password_register">Mot de passe</p>
+                <input type="password" name="register_password">
+            </div>
+
+            <!-- champs Confirmation Password -->                
+            
+            <div class="input_confirmed_password_register" id="class_confirmed_password_register">   
+                <p class="choose_password_register" id="choose_confirmed_password_register">Confirmation du Mot de passe</p>
+                <input type="password" name="register_confirmed_password">
+            </div>
+
+            <!-- champs Captchat -->                
+            
+            <div class="input_captcha" id="class_captcha">   
+                <p class="choose_captcha" id="choose_captcha">Captcha</p>
+                <input type="captcha" id="id_input_choose_captcha" name="register_captcha">
+            </div>
+
+
+            <!-- champs bouton d'inscription -->     
+            <div class="submit_button_register" >
+                <button type="submit" name="register_submit">S'inscrire</button>
+                
+            </div>
+            
 
             
-            <div  class="inputs_left">
+        </div>
+    </form>
+</div>
 
-                <!-- champs Nom -->
-
-                <div class="input_last_name" id="class_choose_nom"> 
-                    <p class="choose_Nom" >Nom</p>    
-                    <input type="last_name" name="register_last_name">
-                </div>     
-
-                <!-- champs Prénom -->
-                
-                <div class="input_first_name" id="class_choose_first_name">
-                    <p class="choose_first_name" >Prénom</p>    
-                    <input type="first_name" name="register_first_name">
-                </div>
-                <!-- champs Email -->
-                
-                <div class="input_email_register" id="class_choose_email_register">
-                    <p class="choose-email_register"  id="choose_email_register">Email</p>    
-                    <input type="email" name="register_email">
-                </div>                
-
-                <!-- champs Classe -->
-                
-                <div class="input_class" id="class_choose_class" >
-                    <p class="choose_class" >Classe</p>    
-                    <input type="class" name="register_class">
-                </div>
-            </div>
-            <div class="div_vertical_line">
-                <span class="vertical_line"></span>
-            </div>
-            <div  class="inputs_right"> 
-
-                <!-- champs Password -->                
-                
-                <div class="input_password_register" id="class_choose_password_register">   
-                    <p class="choose_password_register" id="choose_password_register"> Mot de passe </p>
-                    <input type="password" name="register_password">
-                </div>
-
-                <!-- champs Confirmation Password -->                
-                
-                <div class="input_confirmed_password_register" id="class_confirmed_password_register">   
-                    <p class="choose_password_register" id="choose_confirmed_password_register"> Confirmation du Mot de passe </p>
-                    <input type="password" name="register_confirmed_password">
-                </div>
-
-                <!-- champs Captchat -->                
-                
-                <div class="input_captcha" id="class_captcha">   
-                    <p class="choose_captcha" id="choose_captcha">Captcha</p>
-                    <input type="captcha" id="id_input_choose_captcha" name="register_captcha">
-                </div>
-
-
-                <!-- champs bouton d'inscription -->     
-                <div class="submit_button_register" >
-                    <button type="submit" name="register_submit">S'inscrire</button>
-                    
-                </div>
-                
-
-                
-            </div>
-        </form>
-    </div>
-        
-   
-</body>
-
-</html>
+<!------------------- HTML ------------------->
 
 <?php
 
@@ -118,7 +112,10 @@ try{
     $mdp_hash = password_hash($_POST['register_password'], PASSWORD_DEFAULT);
     $mdp_hash_confirmed = password_hash($_POST['register_confirmed_password'], PASSWORD_DEFAULT);
 
-    if ($mdp_hash === $mdp_hash_confirmed){
+    ECHO($mdp_hash . "\n");
+    echo($mdp_hash_confirmed);
+
+    if (password_verify($_POST['register_password'],$mdp_hash_confirmed)){
 
         $query = $pdo->prepare('INSERT INTO user (USER_NAME, USER_LASTNAME, USER_EMAIL, USER_PASSWORD, USER_ROLE, USER_HND) VALUES (:first_name, :last_name, :email, :psw, :roles, :hdn)');
 
@@ -130,8 +127,7 @@ try{
             'roles' => 'Etudiant',
             'hdn' => $_POST['register_class']
         ]);
-        header('location: login.php');
-        exit();
+        $success = "Votre compte à bien été crée";
 
 
     }else{
@@ -150,3 +146,13 @@ try{
     <?= $erreur?>
 </div>
 <?php endif ?>
+<?php if (isset($success)): ?>
+<div class="alert-success">
+    <?= $success?>
+</div>
+<?php endif ?>
+
+
+<?php require "../elements/footer.php" ?>
+
+
